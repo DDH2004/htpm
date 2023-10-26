28c72a93d97547495d5e5e07faaddb45dce967cd
Shawn Duong <me@shawnd.xyz>
2023-10-26 14:33:05 -0700

    Added conditional redirect for logged in users on the login page.

--- software/web/route.py
+++ software/web/route.py
@@ -3,13 +3,17 @@ from app import *
 from flask_login import current_user, login_required
 
 @app.route("/", methods=["GET"])
-@app.route("/login", methods=["GET"])
 def index():
+
+	return render_template("splash.html")
+
+@app.route("/login", methods=["GET"])
+def loginPage():
 
 	if current_user.is_authenticated:
 		return redirect(url_for("application"))
 
-	return render_template("splash.html")
+	return render_template("login.html")
 
 @app.route("/app", methods=["GET"])
 @login_required
