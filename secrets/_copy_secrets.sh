062c2cfdee37bec891300d8176bb024e2406acfb
Shawn Duong <me@shawnd.xyz>
2023-11-02 12:54:31 -0700

    Renamed move script to copy script.

--- secrets/_copy_secrets.sh
+++ secrets/_copy_secrets.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-echo ":: Moving VPN files to their appropriate targets."
+echo ":: Copying VPN files to their appropriate targets."
 cp "vpn/t0/inst0@t0-htpm-t0.ovpn" ../challenges/0_Smart-Home/t0/
