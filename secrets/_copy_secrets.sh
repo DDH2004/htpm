f9f9c90b4b03346378a141b1d3350e1b0b3a1a8f
shawnduong <me@shawnd.xyz>
2023-11-03 02:53:05 -0700

    Added VPN move for t1-htpm-t0.

--- secrets/_copy_secrets.sh
+++ secrets/_copy_secrets.sh
@@ -7,6 +7,7 @@ cp "vpn/t0/inst0@t0-htpm-d1.ovpn" ../challenges/0_Smart-Home/d1/
 cp "vpn/t0/inst0@t0-htpm-d2.ovpn" ../challenges/0_Smart-Home/d2/
 cp "vpn/t0/inst0@t0-htpm-t0.ovpn" ../challenges/0_Smart-Home/t0/
 cp "vpn/t1/inst1@t1-htpm-d0.ovpn" ../challenges/1_Railroad/d0/
+cp "vpn/t1/inst1@t1-htpm-t0.ovpn" ../challenges/1_Railroad/t0/
 cp "vpn/t2/inst2@t2-htpm-t0.ovpn" ../challenges/2_Power-Grid/t0/
 cp "vpn/t2/inst2@t2-htpm-d0.ovpn" ../challenges/2_Power-Grid/d0/
 cp "vpn/t2/inst2@t2-htpm-d1.ovpn" ../challenges/2_Power-Grid/d1/
