b6f9433ccf2f70839538ee0b9208e178c0eee9d9
shawnduong <me@shawnd.xyz>
2023-11-03 03:29:08 -0700

    Added scorebot to railroad target.

--- challenges/1_Railroad/t0/bootstrap.sh
+++ challenges/1_Railroad/t0/bootstrap.sh
@@ -5,4 +5,5 @@ mknod /dev/net/tun c 10 200
 chmod 600 /dev/net/tun
 openvpn /etc/openvpn/client/inst1\@t1-htpm-t0.ovpn &
 service ssh restart
+python3 /scorebot/main.py &
 while true; do sleep 1000; done
