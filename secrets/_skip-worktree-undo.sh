10ac58a685c181a0ca01ba86f9d3a4894090142d
shawnduong <me@shawnd.xyz>
2023-11-03 00:01:46 -0700

    Fixed wrong extensions on skip worktree scripts.

--- secrets/_skip-worktree-undo.sh
+++ secrets/_skip-worktree-undo.sh
@@ -5,5 +5,5 @@ echo ":: Adding files from this directory back to the worktree."
 git update-index --no-skip-worktree vpn/*/*.ovpn
 git update-index --no-skip-worktree wap/*
 git update-index --no-skip-worktree scorebot/*
-git update-index --no-skip-worktree ../challenges/0_Smart-Home/_firmware/wap/config.ino
-git update-index --no-skip-worktree ../challenges/2_Power-Grid/_firmware/wap/config.ino
+git update-index --no-skip-worktree ../challenges/0_Smart-Home/_firmware/wap/config.h
+git update-index --no-skip-worktree ../challenges/2_Power-Grid/_firmware/wap/config.h
--- secrets/_skip-worktree.sh
+++ secrets/_skip-worktree.sh
@@ -6,5 +6,5 @@ echo ":: accidentally commited to the git repository. Paths are relative."
 git update-index --skip-worktree vpn/*/*.ovpn
 git update-index --skip-worktree wap/*
 git update-index --skip-worktree scorebot/*
-git update-index --skip-worktree ../challenges/0_Smart-Home/_firmware/wap/config.ino
-git update-index --skip-worktree ../challenges/2_Power-Grid/_firmware/wap/config.ino
+git update-index --skip-worktree ../challenges/0_Smart-Home/_firmware/wap/config.h
+git update-index --skip-worktree ../challenges/2_Power-Grid/_firmware/wap/config.h
